3ef0ebdf58e8add4a10ee3c768fc03bacc5294a4
Gabriel-FP <gabrielfelixpaim@gmail.com>
2026-07-09 01:14:42 -0300

    [Infra] Bootstrap Spring Boot 3 project from Initializr

---
+++ src/main/java/br/com/pgerides/PgeRidesApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.pgerides;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PgeRidesApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PgeRidesApiApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/pgerides/PgeRidesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.pgerides;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PgeRidesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
